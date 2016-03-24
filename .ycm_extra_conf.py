c7f590899af84ec464f367b21cbb6ad3dd36a251
zhaozhanxu <zhaozhanxu@163.com>
2016-03-24 14:15:34 +0800

    23333

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -31,41 +31,28 @@
 import os
 import ycm_core
 
-# These are the compilation flags that will be used in case there's no
-# compilation database set (by default, one is not set).
-# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
 '-Wall',
 '-Wextra',
-#'-Werror',
-#'-Wc++98-compat',
+'-Werror',
 '-Wno-long-long',
 '-Wno-variadic-macros',
 '-fexceptions',
-'-stdlib=libc++',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-'-std=c++11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-I',
-'.',
+'-DNDEBUG',
+'-DUSE_CLANG_COMPLETER',
+'-std=c99',
+'-isystem',
+'../BoostParts',
+'-isystem',
+'../llvm/include',
+'-isystem',
+'../llvm/tools/clang/include',
+'-isystem',
+'./ClangCompleter',
 '-isystem',
 '/usr/include',
 '-isystem',
 '/usr/local/include',
-'-isystem',
-'/Library/Developer/CommandLineTools/usr/include',
-'-isystem',
-'/Library/Developer/CommandLineTools/usr/bin/../lib/c++/v1',
 ]
 
 
@@ -156,13 +143,6 @@ def FlagsForFile( filename, **kwargs ):
       compilation_info.compiler_flags_,
       compilation_info.compiler_working_dir_ )
 
-    # NOTE: This is just for YouCompleteMe; it's highly likely that your project
-    # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
-    # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
-    #try:
-    #  final_flags.remove( '-stdlib=libc++' )
-    #except ValueError:
-    #  pass
   else:
     relative_to = DirectoryOfThisScript()
     final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
@@ -171,3 +151,4 @@ def FlagsForFile( filename, **kwargs ):
     'flags': final_flags,
     'do_cache': True
   }
+
--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 2. mkdir ~/.vim/bundle
 3. git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 4. push button F6 to install plugins in vim
-5. sudo apt-get install cmake
-6. cd ~/.vim/bundle/YouCompleteMe
-7. ./install.py --clang-completer --gocode-completer
-8. cd ~/.vim/bundle/YCM-Generator/
-9. ./config_gen.py {work_dir}
+5. sudo apt-get install cmake python-dev build-essential
+6. git clone https://github.com/powerline/fonts.git
+7. cd fonts
+8. ./install.sh
+9. cd ~/.vim/bundle/YouCompleteMe
+10. ./install.py --clang-completer --gocode-completer
+11. cd ~/.vim/bundle/YCM-Generator/
+12. ./config_gen.py {work_dir}
